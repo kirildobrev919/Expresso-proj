cce003838a2b3d37211dfe674b210e85bc210d75
kirildobrev919 <kirildobrev919@gmail.com>
2020-10-13 12:13:09 +0100

    port fixed in server js

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
-const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const errorhandler = require('errorhandler');
+const express = require('express');
 const morgan = require('morgan');
 
+const apiRouter = require('./api/api.js');
+
 const app = express();
-const PORT = process.env.PORT || 400;
+const PORT = process.env.PORT || 4000;
 
 //app usage of package helpers
 app.use(bodyParser.json());
 app.use(cors());
-app.use(errorhandler());
+
 //morgan depending on env
 if (app.get('env') !== 'production') {
     app.use(morgan('dev'));
-} else {
-    app.use(morgan('tiny'));
 }
 
-const apiRouter = require('./api/api.js');
+
 app.use('/api', apiRouter);
+app.use(errorhandler());
 
 app.listen(PORT, () => {
     console.log(`App listen on port ${PORT}!`);
